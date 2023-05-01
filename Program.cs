1cd2157f76672ceba743b10344b84aa289ac8f40
Daniel Ajenifuja <335984019@educ.dpcdsb.org>
2023-04-30 23:46:16 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace translation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a phrase:");
+            string userInput = Console.ReadLine();
+            string vowels = "aeiouAEIOU";
+            string consonants = "";
+
+            // Check vowel first index
+            if (vowels.Contains(userInput[0]))
+            {
+                Console.WriteLine(userInput + "yay");
+            }
+            else
+            {
+                // find index
+                int vowelIndex = -1;
+                for (int i = 0; i < userInput.Length; i++)
+                {
+                    if (vowels.Contains(userInput[i]))
+                    {
+                        vowelIndex = i;
+                        break;
+                    }
+                }
+
+                // final inputs
+                string endconsonant = userInput.Substring(0, vowelIndex);
+                string newString = userInput.Substring(vowelIndex) + endconsonant + "ay";
+                Console.WriteLine("Pig Latin: " + newString);
+            }
+        }
+    }
+}
+
